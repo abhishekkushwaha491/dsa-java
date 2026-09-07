a13b8e6f24c49c32f356ba343be09079534fe8e9
ABHISHEK KUSHWAHA <abhi2008kushwaha@gmail.com>
2026-09-08 00:43:37 +0530

    Solve linear search problem
    Implemented linear search algorithm to find the index of a target element in an array.

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int search(int[] arr, int x) {
+        // Traverse the array and return the index when the target is found.
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
